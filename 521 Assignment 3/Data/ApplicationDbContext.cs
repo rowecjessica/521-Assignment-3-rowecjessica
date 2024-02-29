0a046e77f38c987d2c68c5c35a6fd96442f651e9
rowecjessica <jcrowe@crimson.ua.edu>
2024-02-29 16:50:22 -0600

    Add project files.

---
+++ 521 Assignment 3/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using _521_Assignment_3.Models;
+
+namespace _521_Assignment_3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<_521_Assignment_3.Models.Movies> Movies { get; set; } = default!;
+    }
+}
---
+++ 521 Assignment 3/Models/Movies.cs
@@ -0,0 +1,10 @@
+﻿namespace _521_Assignment_3.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
